65a1c6cc475e40bcf565be7746e72c8e4f49ce54
maneesh-yadav <maneesh.yadav@adevinta.com>
2024-11-26 08:54:28 +0100

    Initial Commit

---
+++ LocationExplorer/LocationExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationExplorerApp.swift
+//  LocationExplorer
+//
+//  Created by Maneesh Yadav on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocationExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
